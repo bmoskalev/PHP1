0a92967d43e0dcf9f29c9c6a382ced106bdcd9d9
bmoskalev <cODE2TEACH!>
2018-12-21 00:30:25 +0500

    Initial commit

---
+++ 4.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/html;charset=utf-8');
+$title = "Title1";
+$context = "Some text for h1"
+?>
+<h1><?php echo $title;?></h1>
+<p><?php echo $context;?></p>
+<p><?php echo date("Y");?></p>
+
---
+++ 5.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Москалевы
+ * Date: 20.12.2018
+ * Time: 23:03
+ */
+$a = 5;
+$b = 2;
+echo "\$a=$a \$b=$b" . "<br>";
+$a = $a + $b;
+$b = $a - $b;
+$a = $a - $b;
+echo "\$a=$a \$b=$b";
+
