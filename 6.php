b310676d0db971728f72b6966d4f1a1f92785af8
bmoskalev <cODE2TEACH!>
2018-12-25 23:18:02 +0500

    Fix bugs in power func

--- 6.php
+++ 6.php
@@ -8,8 +8,10 @@
 
 function power($val, $pow)
 {
-    if ($pow == 1) {
-        return $val;
+    if ($pow == 0)
+        return 1;
+    if ($pow < 0) {
+        return power(1 / $val, -$pow);
     } else {
         return $val * power($val, $pow - 1);
     }
