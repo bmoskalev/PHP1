ab6dd4967d2b86e8c3810498dc8db91dccdce45c
bmoskalev <cODE2TEACH!>
2018-12-27 09:11:40 +0500

    Add 5 task

--- 5.php
+++ 5.php
@@ -5,14 +5,6 @@
  * Date: 27.12.2018
  * Time: 8:50
  */
-function replSpace($inStr){
-    echo mb_strlen($inStr);
-    for ($i = 0; $i < mb_strlen($inStr); $i++) {
-        if ($inStr[$i]==' '){
-            $inStr[$i]='_';
-        }
-    }
-    return $inStr;
-}
-$a="Привет  страна наша Россия!";
-echo replSpace($a);
+
+$a="Привет страна наша  Russia!";
+echo str_replace(" ","_",$a);
